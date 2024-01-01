31e28541b10ee96e2369f9c8f726611af4c7aba9
Diwas15 <89520486+Diwas15@users.noreply.github.com>
2024-01-01 16:44:32 +0530

    Updated sync notes in server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/',(req,res)=>{
 app.post('/notes/new',(req,res)=>{
         const email=req.body.user;
         const data = Messages.find({user_email:email}).exec()
-        .then((data)=>res.status(200).send("bhai server to chlra hai"))
+        .then((data)=>res.status(200).send(data))
         .catch((e)=>console.log("lo error aa  gaya"));
 });
 
